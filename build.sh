57e063eb818d23349834e9eaa7f4768b7990f3d4
Llucs <104036495+Llucs@users.noreply.github.com>
2025-11-18 22:57:42 -0300

    Atualizar o build.sh

--- build.sh
+++ build.sh
@@ -4,10 +4,10 @@ set -e
 PROFILE_DIR="$(pwd)"
 
 echo "[*] Limpando build anterior…"
-sudo rm -rf work out
+rm -rf work out
 
 echo "[*] Iniciando build da SpeedOS…"
-sudo mkarchiso -v -w work -o out "$PROFILE_DIR"
+mkarchiso -v -w work -o out "$PROFILE_DIR"
 
 echo "[✔] ISO compilada com sucesso!"
 echo "Arquivo gerado em: out/"
