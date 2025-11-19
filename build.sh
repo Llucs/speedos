45ed89cb1a987201cc6c01206b77b8204f067907
Llucs <104036495+Llucs@users.noreply.github.com>
2025-11-19 15:45:21 -0300

    Atualizar o build.sh

--- build.sh
+++ build.sh
@@ -6,8 +6,8 @@ PROFILE_DIR="$(pwd)"
 echo "[*] Limpando build anterior…"
 rm -rf work out
 
-echo "[*] Instalando dependências base e compilando pacotes AUR…"
-sudo pacman -Syu --noconfirm base-devel git
+echo "[*] Instalando dependências base…"
+sudo pacman -Syu --noconfirm base-devel git wget
 
 # -----------------------------
 # Clonar e instalar o kernel Liquorix
@@ -32,6 +32,49 @@ for pkg in "${AUR_PKGS[@]}"; do
   makepkg -si --noconfirm
   cd ..
 done
+
+# -----------------------------
+# Instalar pacotes do GitHub (Arc, Tela, Calamares)
+# -----------------------------
+
+# Arc GTK Theme
+if [ ! -d "arc-gtk-theme" ]; then
+    git clone https://github.com/horst3180/arc-theme.git arc-gtk-theme
+fi
+cd arc-gtk-theme
+./autogen.sh --prefix=/usr
+make
+sudo make install
+cd ..
+
+# Tela Icon Theme
+if [ ! -d "tela-icon-theme" ]; then
+    git clone https://github.com/vinceliuice/Tela-icon-theme.git
+fi
+cd Tela-icon-theme
+./install.sh
+cd ..
+
+# Calamares + Extensions
+if [ ! -d "calamares" ]; then
+    git clone https://github.com/calamares/calamares.git
+fi
+cd calamares
+mkdir -p build && cd build
+cmake .. -DCMAKE_INSTALL_PREFIX=/usr
+make -j$(nproc)
+sudo make install
+cd ../..
+
+if [ ! -d "calamares-extensions" ]; then
+    git clone https://github.com/calamares/calamares-extensions.git
+fi
+cd calamares-extensions
+mkdir -p build && cd build
+cmake .. -DCMAKE_INSTALL_PREFIX=/usr
+make -j$(nproc)
+sudo make install
+cd ../..
 
 # -----------------------------
 # Iniciar build da ISO
